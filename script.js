8658a44f068d6264c2eb394abfa199fb3d51c4cf
Fall <62287602+FallYT@users.noreply.github.com>
2024-12-04 11:27:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// JavaScript to handle the page load and animation transition
-window.onload = () => {
-  const timeout = setTimeout(() => {
-    // Remove 'not-loaded' class and add 'loaded' class to trigger animation
-    document.body.classList.remove("not-loaded");
-    document.body.classList.add("loaded");
-    clearTimeout(timeout);
-  }, 1000); // 1-second delay before the flowers start to "bloom"
-};
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
